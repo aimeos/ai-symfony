981b5bd521dbb35cc2f8cd9fd349fcb7eeac1e06
Aimeos <aimeos@aimeos.org>
2016-01-04 23:32:33 +0100

    Implemented getUploadedFiles() in request view helper

--- lib/custom/src/MW/View/Helper/Request/Symfony2.php
+++ lib/custom/src/MW/View/Helper/Request/Symfony2.php
@@ -11,6 +11,9 @@
 namespace Aimeos\MW\View\Helper\Request;
 
 
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+
 /**
  * View helper class for retrieving data from Symfony2 requests.
  *
@@ -18,7 +21,7 @@ namespace Aimeos\MW\View\Helper\Request;
  * @subpackage View
  */
 class Symfony2
-	extends \Aimeos\MW\View\Helper\Base
+	extends \Aimeos\MW\View\Helper\Request\Standard
 	implements \Aimeos\MW\View\Helper\Request\Iface
 {
 	private $request;
@@ -30,54 +33,40 @@ class Symfony2
 	 * @param \Aimeos\MW\View\Iface $view View instance with registered view helpers
 	 * @param \Symfony\Component\HttpFoundation\Request $request Symfony2 request object
 	 */
-	public function __construct( $view, \Symfony\Component\HttpFoundation\Request $request )
+	public function __construct( $view, \Symfony\Component\HttpFoundation\Request $req )
 	{
-		parent::__construct( $view );
-
-		$this->request = $request;
+		parent::__construct( $view, $req->getContent(), $req->getClientIp(), $req->get( '_route' ), $req->files );
 	}
 
 
 	/**
-	 * Returns the request view helper.
+	 * Creates a normalized file upload data from the given array.
 	 *
-	 * @return \Aimeos\MW\View\Helper\Iface Request view helper
+	 * @param \Traversable|array $files File upload data
+	 * @return array Multi-dimensional list of file objects
 	 */
-	public function transform()
+	protected function createUploadedFiles( $files )
 	{
-		return $this;
-	}
+		$files = array();
 
+		foreach( $files as $key => $value )
+		{
+			if( $value instanceof UploadedFile )
+			{
+				$files[$key] = new \Aimeos\MW\View\Helper\Request\File\Standard(
+					$value->getRealPath(),
+					$value->getClientOriginalName(),
+					$value->getSize(),
+					$value->getClientMimeType(),
+					$value->getError()
+				);
+			}
+			else
+			{
+				$files[$key] = $this->createUploadedFiles($value);
+			}
+		}
 
-	/**
-	 * Returns the request body.
-	 *
-	 * @return string Request body
-	 */
-	public function getBody()
-	{
-		return $this->request->getContent();
-	}
-
-
-	/**
-	 * Returns the client IP address.
-	 *
-	 * @return string Client IP address
-	 */
-	public function getClientAddress()
-	{
-		return $this->request->getClientIp();
-	}
-
-
-	/**
-	 * Returns the current page or route name
-	 *
-	 * @return string|null Current page or route name
-	 */
-	public function getTarget()
-	{
-		return $this->request->get( '_route' );
+		return $files;
 	}
 }
--- lib/custom/tests/MW/View/Helper/Request/Symfony2Test.php
+++ lib/custom/tests/MW/View/Helper/Request/Symfony2Test.php
@@ -13,65 +13,72 @@ class Symfony2Test extends \PHPUnit_Framework_TestCase
 	private $mock;
 
 
-	/**
-	 * Sets up the fixture, for example, opens a network connection.
-	 * This method is called before a test is executed.
-	 *
-	 * @access protected
-	 */
 	protected function setUp()
 	{
 		if( !class_exists( '\Symfony\Component\HttpFoundation\Request' ) ) {
 			$this->markTestSkipped( '\Symfony\Component\HttpFoundation\Request is not available' );
 		}
-
-		$view = new \Aimeos\MW\View\Standard();
-		$this->mock = $this->getMock( '\Symfony\Component\HttpFoundation\Request' );
-		$this->object = new \Aimeos\MW\View\Helper\Request\Symfony2( $view, $this->mock, array() );
-	}
-
-
-	/**
-	 * Tears down the fixture, for example, closes a network connection.
-	 * This method is called after a test is executed.
-	 *
-	 * @access protected
-	 */
-	protected function tearDown()
-	{
-		unset( $this->object, $this->mock );
 	}
 
 
 	public function testTransform()
 	{
-		$this->assertInstanceOf( '\\Aimeos\\MW\\View\\Helper\\Request\\Symfony2', $this->object->transform() );
+		$view = new \Aimeos\MW\View\Standard();
+		$mock = $this->getMock( '\Symfony\Component\HttpFoundation\Request' );
+		$object = new \Aimeos\MW\View\Helper\Request\Symfony2( $view, $mock, array() );
+
+		$this->assertInstanceOf( '\\Aimeos\\MW\\View\\Helper\\Request\\Symfony2', $object->transform() );
 	}
 
 
 	public function testGetBody()
 	{
-		$this->mock->expects( $this->once() )->method( 'getContent' )
+		$view = new \Aimeos\MW\View\Standard();
+		$mock = $this->getMock( '\Symfony\Component\HttpFoundation\Request' );
+
+		$mock->expects( $this->once() )->method( 'getContent' )
 			->will( $this->returnValue( 'body' ) );
 
-		$this->assertEquals( 'body', $this->object->transform()->getBody() );
+		$object = new \Aimeos\MW\View\Helper\Request\Symfony2( $view, $mock, array() );
+
+		$this->assertEquals( 'body', $object->transform()->getBody() );
 	}
 
 
 	public function testGetClientAddress()
 	{
-		$this->mock->expects( $this->once() )->method( 'getClientIp' )
+		$view = new \Aimeos\MW\View\Standard();
+		$mock = $this->getMock( '\Symfony\Component\HttpFoundation\Request' );
+
+		$mock->expects( $this->once() )->method( 'getClientIp' )
 			->will( $this->returnValue( '127.0.0.1' ) );
 
-		$this->assertEquals( '127.0.0.1', $this->object->transform()->getClientAddress() );
+		$object = new \Aimeos\MW\View\Helper\Request\Symfony2( $view, $mock, array() );
+
+		$this->assertEquals( '127.0.0.1', $object->transform()->getClientAddress() );
 	}
 
 
 	public function testGetTarget()
 	{
-		$this->mock->expects( $this->once() )->method( 'get' )
+		$view = new \Aimeos\MW\View\Standard();
+		$mock = $this->getMock( '\Symfony\Component\HttpFoundation\Request' );
+
+		$mock->expects( $this->once() )->method( 'get' )
 			->will( $this->returnValue( 'test' ) );
 
-		$this->assertEquals( 'test', $this->object->transform()->getTarget() );
+		$object = new \Aimeos\MW\View\Helper\Request\Symfony2( $view, $mock, array() );
+
+		$this->assertEquals( 'test', $object->transform()->getTarget() );
+	}
+
+
+	public function testGetUploadedFiles()
+	{
+		$view = new \Aimeos\MW\View\Standard();
+		$mock = $this->getMock( '\Symfony\Component\HttpFoundation\Request' );
+		$object = new \Aimeos\MW\View\Helper\Request\Symfony2( $view, $mock, array() );
+
+		$this->assertEquals( array(), $object->transform()->getUploadedFiles() );
 	}
 }
